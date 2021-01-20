d10497aa9af7903502656b82d63b4c57d1c075df
Artem Orlov <universalism123@gmail.com>
2021-01-20 11:48:54 +0200

    main functionality is completed

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const stream = require('stream');
+const { ObjectId } = require('mongodb');
 
 // create storage for images
 const storage = new multer.diskStorage({
@@ -29,10 +30,6 @@ const conn = mongoose.createConnection(mongoURI, {
 
 const router = express.Router();
 
-router.get('/uploads', (req, res) => {
-    res.send();
-})
-
 router.get('/uploads/:filename', (req, res) => {
     const read = fs.createReadStream('./backend/routes/api' + req.url);
 
@@ -57,15 +54,34 @@ router.get('/', async (req, res) => {
     res.send(await posts.find().toArray());
 });
 
+router.get('/postDetails/:postId', async (req, res) => {
+    const posts = await loadPostsCollection();
+
+    return posts.findOne({ "_id": ObjectId(req.params.postId) })
+        .then(result => {
+            return res.send(result);
+        })
+        .catch(err => console.log('Fail to find! ', err));
+});
+
 // POST posts
-router.post('/', upload.single('file'), async (req, res) => {
+router.post('/', upload.array('files', 15), async (req, res) => {
     const posts = await loadPostsCollection();
 
     await posts.insertOne({
         heading: req.body.heading,
+        description: req.body.description,
+        firstName: req.body.firstname,
+        lastName: req.body.lastname,
         price: req.body.price,
-        address: req.body.address,
-        file: req.file,
+        country: req.body.country,
+        state: req.body.state,
+        city: req.body.city,
+        street: req.body.street,
+        houseNumber: req.body.house,
+        zip: req.body.zip,
+        file: req.files.shift(),
+        files: req.files,
     });
 
     res.status(201).send();
@@ -73,16 +89,6 @@ router.post('/', upload.single('file'), async (req, res) => {
 
 // loading to Mongo DB
 async function loadPostsCollection() {
-    {// const client = await MongoDB.MongoClient.connect(
-    //     'mongodb+srv://artem123:qazwsx12@cluster0.4coc6.mongodb.net/estate', 
-    //     {
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true
-    //     }
-    //     );
-    }
-
-    // return client.db('estate').collection('posts');
     return (await conn).collection('posts');
 }
 
